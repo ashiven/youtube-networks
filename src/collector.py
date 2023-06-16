5ec168161daa43aeefb51dfae3ae6511e1513730
Gunnar Scherdin <gunnar.scherdin@campus.tu-berlin.de>
2023-06-16 18:18:17 +0200

    added yet another api key

--- src/collector.py
+++ src/collector.py
@@ -496,22 +496,23 @@ def main():
     apiKey2 = '***REMOVED***'     # Jonathan
     apiKey3 = '***REMOVED***'     # Gunnar
     apiKey4 = '***REMOVED***'     # Gunnar
-    apiKey5 = '***REMOVED***'     # Elena
+    apiKey5 = '***REMOVED***'     # Gunnar
     apiKey6 = '***REMOVED***'     # Elena
     apiKey7 = '***REMOVED***'     # Elena
     apiKey8 = '***REMOVED***'     # Elena
     apiKey9 = '***REMOVED***'     # Elena
     apiKey10 = '***REMOVED***'    # Elena
-    apiKey11 = '***REMOVED***'    # Egemen
+    apiKey11 = '***REMOVED***'    # Elena
     apiKey12 = '***REMOVED***'    # Egemen
-    apiKey13 = 'AIzaSyC-DFQ4ayefiTyuikxAw81DQJUIK6a5Cdc'    # Jonathan
-    apiKey14 = 'AIzaSyDTjdiFBRlTz0WoChHp08DMew7epq2MkPQ'    # Jonathan
-    apiKey15 = 'AIzaSyCsablwpeXxJ5FtuOQRbei1WNmloGR04D8'    # Jonathan
-    apiKey16 = 'AIzaSyB-ZH3P_j-bmhNsNgF9gOy05L8lTZ3M7C4'    # Jonathan
-    apiKey17 = 'AIzaSyBNNCbn7QOHPn7OY9UfUhR_FGPfhVpIo1I'    # Jonathan
+    apiKey13 = '***REMOVED***'    # Egemen
+    apiKey14 = 'AIzaSyC-DFQ4ayefiTyuikxAw81DQJUIK6a5Cdc'    # Jonathan
+    apiKey15 = 'AIzaSyDTjdiFBRlTz0WoChHp08DMew7epq2MkPQ'    # Jonathan
+    apiKey16 = 'AIzaSyCsablwpeXxJ5FtuOQRbei1WNmloGR04D8'    # Jonathan
+    apiKey17 = 'AIzaSyB-ZH3P_j-bmhNsNgF9gOy05L8lTZ3M7C4'    # Jonathan
+    apiKey18 = 'AIzaSyBNNCbn7QOHPn7OY9UfUhR_FGPfhVpIo1I'    # Jonathan
 
     apiKeys = [apiKey0, apiKey1, apiKey2, apiKey3, apiKey4, apiKey5, apiKey6, apiKey7, apiKey8,
-               apiKey9, apiKey10, apiKey11, apiKey12, apiKey13, apiKey14, apiKey15, apiKey16, apiKey17]
+               apiKey9, apiKey10, apiKey11, apiKey12, apiKey13, apiKey14, apiKey15, apiKey16, apiKey17,apiKey18]
 
 
 
